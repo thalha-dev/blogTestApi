b28847b988ac8d2018b579eae935d74f1d054329
thalha-dev <dev.thalha@gmail.com>
2024-04-24 11:32:26 +0530

    initial files

---
+++ src/main/java/com/its/blogTestApi/BlogTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.its.blogTestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/its/blogTestApi/BlogTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.blogTestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
