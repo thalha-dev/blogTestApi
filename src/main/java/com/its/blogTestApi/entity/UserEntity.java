8e383fb7bc30a8979b5308c5cd12672184c12a20
thalha-dev <dev.thalha@gmail.com>
2024-04-27 12:09:36 +0530

    remove unused imports

--- src/main/java/com/its/blogTestApi/entity/UserEntity.java
+++ src/main/java/com/its/blogTestApi/entity/UserEntity.java
@@ -1,7 +1,5 @@
 package com.its.blogTestApi.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
