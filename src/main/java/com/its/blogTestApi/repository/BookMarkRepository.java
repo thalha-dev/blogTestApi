500cc8611b7c9b76ac3cb94e88075b405da8ac73
thalha-dev <dev.thalha@gmail.com>
2024-04-30 11:44:03 +0530

    update repo

--- src/main/java/com/its/blogTestApi/repository/BookMarkRepository.java
+++ src/main/java/com/its/blogTestApi/repository/BookMarkRepository.java
@@ -1,5 +1,7 @@
 package com.its.blogTestApi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.its.blogTestApi.entity.BookMarkEntity;
 
 @Repository
 public interface BookMarkRepository extends JpaRepository<BookMarkEntity, Long> {
+
+  Optional<BookMarkEntity> findByUserIdAndBlogId(Long userId, Long blogId);
 
 }
